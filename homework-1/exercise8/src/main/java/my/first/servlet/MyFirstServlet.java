eccfaa8c9e30953109d5e47f6a25f9c0e743f025
Twisterly <98797561+Twisterly@users.noreply.github.com>
2022-10-15 11:07:34 +0300

    Create MyFirstServlet.java

---
+++ homework-1/exercise8/src/main/java/my/first/servlet/MyFirstServlet.java
@@ -0,0 +1,33 @@
+package my.first.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet(name = "MyFirstServlet", urlPatterns = "/my-servlet")
+public class MyFirstServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>First Servlet</title></head>");
+        out.println("<body><h1>This is First Servlet</h1>");
+        out.println("</body></html>");
+
+    }
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
