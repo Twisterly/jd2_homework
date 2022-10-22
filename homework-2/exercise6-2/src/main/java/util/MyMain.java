6688f08abe3aaf58e379bbf6d39431727ee38217
Masha Chachik <98797561+Twisterly@users.noreply.github.com>
2022-10-22 23:38:30 +0300

    Create MyMain.java

---
+++ homework-2/exercise6-2/src/main/java/util/MyMain.java
@@ -0,0 +1,12 @@
+package masha.cha.util;
+
+
+public class MyMain {
+    public static void main(String[] args) {
+        DbManager.getReceiversAndSum();
+        DbManager.getSumOfLargestPaymentDay();
+        DbManager.getLargestPayment();
+
+
+    }
+}
